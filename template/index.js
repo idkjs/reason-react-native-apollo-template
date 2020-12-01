9e76202083a6757133a0c79759f65520ae1e5fd4
Alain Armand <aarmand.inbox@gmail.com>
2020-12-01 15:20:39 +0100

    update so Podfile works

--- template/index.js
+++ template/index.js
@@ -2,9 +2,14 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
+/**
+ * @format
+ */
+
+import { AppRegistry } from "react-native";
+import App from "./App";
 
 const APP_NAME = "ProjectName";
 
 AppRegistry.registerComponent(APP_NAME, () => App);
+
